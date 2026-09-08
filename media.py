3fdc05bff93554c05231c4e82d5df2557b62a35b
202601400983 <stardurstd@gmail.com>
2026-09-08 20:48:50 -0300

    Atividade de Média de notas em Python

---
+++ media.py
@@ -0,0 +1,11 @@
+#Variaveis de notas
+nota1 = 8
+nota2 = 9
+nota3 = 10
+nota4 = 5
+
+#Operação de média aritimética
+md = (nota1 + nota2 + nota3 + nota4) / 4
+
+#Impressão da média final
+print("\nSua média foi igual a: ",md)
